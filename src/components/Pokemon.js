6dc63ae2698d00f7c44c8683645234d797007525
404-lorena <lramirezlopez@google.com>
2023-07-21 09:37:56 -0400

    Installed React Bootstrap

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+
 const Pokemon = () => {
 
         return (
-            <div>
+            <div className="container">
                 <h1>Gotta catch 'em all!</h1> 
+                <div className="pokemon-container">
+                    <div className="all-container">
+                        <Button variant="primary">Load More</Button>
+                    </div>
+
+                </div>
             </div>
         )
     }
